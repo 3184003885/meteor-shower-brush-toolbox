c6add78bdd5f874a722d20c16ba8e2f93b6ea9d0
3184003885 <3184003885@qq.com>
2025-07-31 05:16:17 +0800

    Add files via upload

---
+++ config/about.py
@@ -0,0 +1,15 @@
+AUTHOR_INFO = {
+    "作者": "流星雨",
+    "QQ": "3184003885",
+    "主页": "https://yulovehan.top"
+}
+
+DESCRIPTION = """本工具箱集成了各种安卓刷机和玩机工具
+提供了便捷的图形界面
+支持ROOT、刷机、文件管理等功能
+工具免费，禁止商用
+如果觉得工具好用
+欢迎分享给身边的朋友
+也可以通过以下方式支持我的开发工作"""
+
+SPONSOR_URL = "https://yulovehan.top/sponsor.php"
